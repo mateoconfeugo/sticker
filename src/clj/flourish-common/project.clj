cd4c83a4495a13a6b47b3cffedc133ea54da7a2e
Matt Burns <matthewburns@gmail.com>
2013-04-01 13:08:06 -0700

    adding a common clojure library and removing untracked files

---
+++ src/clj/flourish-common/project.clj
@@ -0,0 +1,5 @@
+(defproject flourish-common "0.1.0-SNAPSHOT"
+  :description "common utility functions"
+  :aot :all
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [cheshire "5.0.2"]])
