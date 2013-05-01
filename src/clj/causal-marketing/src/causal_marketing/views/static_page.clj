844e8982ad413b4e2f6b624129204c7d7993c3f8
Matt Burns <matthewburns@gmail.com>
2013-05-01 12:19:31 -0700

    removed unnecessary dom elements, replaced div with html5 semantic equivalent changed the enlive templates to follow added a gusto style sheet

--- src/clj/causal-marketing/src/causal_marketing/views/static_page.clj
+++ src/clj/causal-marketing/src/causal_marketing/views/static_page.clj
@@ -7,12 +7,43 @@
 
 (deftemplate static-html-page "templates/index.html"
   [{:keys [site-name html menu-data]}]
-  [:#header] (html/content (nav-bar {:title site-name :menu-data menu-data}))
-  [:#rootwizard] (str html))
+  [:div#navbar] (html/content (nav-bar {:title site-name :menu-data menu-data}))
+  [:#rootwizard] (html/substitute (html/html-snippet html))
+  [:footer] (html/content "")
+  [:#sidebar] (html/content ""))
 
-(defn render
+(defn render-static-page
   ([file cms id]
      (let [menu (:drop_down_menu (first (get-site-menu cms id)))
            html (slurp file)]     
-       (render-to-response (static-html-page {:site-name "Causal Marketing" :html html :menu-data menu})))))
+       (render-to-response (static-html-page {:site-name "Market With Gusto" :html html :menu-data menu})))))
 
+(comment
+
+(deftemplate index-with-webapp-pages "templates/index.html"
+  [{:keys [site-name pages menu-data]}]
+  [:#header] (html/content (nav-bar {:title site-name :menu-data menu-data}))
+  [:ul.pages :li] (clone-for [p pages]
+                             [:a] (do->
+                                   (add-class "btn")
+                                   (add-class "btn-success")
+                                   (set-attr :href (str "#tab" (:order p)))
+                                   (set-attr :data-toggle "tab")
+                                   (content (:header p))))
+  [:div.tab-content :div.tab-pane] (clone-for [p pages]
+                                              (do->
+                                               (set-attr :id (str "tab" (:order p)))
+                                               (html-content (:contents p))))
+  [[:ul.pages (nth-of-type 1)] :> first-child] (add-class "active")
+  [[:.tab-pane first-child]] (add-class "active"))
+
+(defn render
+  [id cms]
+  "Take the sequence of pages in insert them into an unordered list"
+  (let [menu (:drop_down_menu (first (get-site-menu cms id)))
+        pages (get-site-contents cms id)
+        num_pages (count pages)
+        page_num (range 0 num_pages)
+        pages (reverse (map #(assoc %1 :order %2)  pages page_num))]
+    (render-to-response (index-with-webapp-pages {:site-name "MarketWithGusto.com" :pages pages :menu-data menu}))))
+)
