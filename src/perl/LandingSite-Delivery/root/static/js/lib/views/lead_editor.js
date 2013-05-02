6a13ba82fb610e2a93dbb97af6414c4a19a9257e
Matt Burns <matthewburns@gmail.com>
2013-05-02 10:25:41 -0700

    fixed a problem with form caused by a regress need tests

--- src/perl/LandingSite-Delivery/root/static/js/lib/views/lead_editor.js
+++ src/perl/LandingSite-Delivery/root/static/js/lib/views/lead_editor.js
@@ -17,15 +17,16 @@ define(
 		this.model = new Lead();
 		this.model.on("sync", this.render);
 		this.render();
+
 		$.validator.addMethod("phoneUS", function(phone_number, element) {
 		    phone_number = phone_number.replace(/\s+/g, ""); 
 		    return this.optional(element) || phone_number.length > 9 &&
 			phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
 		}, "Please specify a valid phone number");
+
 		return this;
 	    },
 	    render: function(e) {
-		this.$el.html(Jemplate.process(this.template));
 		$("#lead_form").validate(this.model.validation);
 		return this;
 	    },
@@ -53,6 +54,7 @@ define(
 			$('.modal-backdrop').remove();
 			$('#lead_form_link').remove();
 			$('#rootwizard').html(Jemplate.process("thank_you.tt"));			
+			$('aside#side-lead-form').remove();
 		    },
 		    error: function(e, response) {
 			var json = JSON.parse(response.responseText);
